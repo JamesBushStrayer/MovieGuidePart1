4366d4a17ba6460670fbd9ff0b5d500affa2b2fa
username <useremail>
2023-08-28 01:53:45 -0700

    Final

--- MovieGuidePart1.py
+++ MovieGuidePart1.py
@@ -2,9 +2,6 @@
 # CIS261002VA016-1236-001
 # Lab: Movie Guide Part 1
 
-from msilib import MSIMODIFY_VALIDATE_DELETE
-
-
 def movie_guide():
     print("The Movie List program\n")
     print("COMMAND MENU")
@@ -15,20 +12,20 @@ def movie_guide():
     
 def list(movie_list):
     for i, movie in enumerate(movie_list, start = 1):
-        print(f"(i). (movie)\n")
+        print(f"{i}. {movie}\n")
         
 def add(movie_list):
     movie = input("Name: ")
     movie_list.append(movie)
-    print(f"(movie) was added.\n")
+    print(f"{movie} was added.\n")
     
 def delete(movie_list):
-    number = Int(input("Number: "))
+    number = int(input("Number: "))
     if number < 1 or number > len(movie_list):
         print("Invalid movie number.\n")
     else:
         movie = movie_list.pop(number - 1)
-        print(f"(movie) was deleted.\n")
+        print(f"{movie} was deleted.\n")
         
 def main():
     movie_list = ["Star Wars", "Raiders of the Lost Ark", "E.T."]
@@ -42,10 +39,10 @@ def main():
         elif command.lower() == "del":
             delete(movie_list)
         elif command.lower() == "exit":
+            print("Bye!")
             break
         else:
             print("Not a valid command. Please try again.")
-    print("Bye!")
     
 if __name__ == "__main__":
     main()
